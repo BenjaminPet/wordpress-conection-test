c30b9496891850b6a0472eb1dd68b7ed6a0075ad
Hacker_Master01 <56979864+BenjaminPet@users.noreply.github.com>
2023-03-30 10:13:03 +0200

    function register gutenburg block 2

--- twentytwentythree/functions.php
+++ twentytwentythree/functions.php
@@ -8,7 +8,7 @@ function hfm_acf_init_block_types() {
     if (function_exists('acf_register_block_type')) {
         
         // register our custom block
-        scf_register_block_type( array(
+        acf_register_block_type( array(
             'name' => 'test',
             'title' => 'test',
             'description' => 'test',
