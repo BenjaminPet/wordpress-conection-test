786a9a4b62605431df30efc055600b9397acf1b3
Hacker_Master01 <56979864+BenjaminPet@users.noreply.github.com>
2023-03-30 10:30:30 +0200

    debug 9

--- twentytwentythree/functions.php
+++ twentytwentythree/functions.php
@@ -6,20 +6,20 @@ function register_acf_blocks() {
 
 
 //add_action( 'acf/init', 'hfm_acf_init_block_types' );
-function hfm_acf_init_block_types() {
-    if (function_exists('acf_register_block_type')) {
+//function hfm_acf_init_block_types() {
+//    if (function_exists('acf_register_block_type')) {
         
         // register our custom block
-        acf_register_block_type( array(
-            'name' => 'text',
-            'title' => 'text',
-            'description' => 'text',
-            'render_template' => __dir__ . 'blocks/text/',
-            'category' => 'formatting',
-            'icon' => 'admin-comments',
-            'keywords' => array('text')
+  //      acf_register_block_type( array(
+    //        'name' => 'text',
+      //      'title' => 'text',
+        //    'description' => 'text',
+          //  'render_template' => __dir__ . 'blocks/text/',
+   //         'category' => 'formatting',
+     //       'icon' => 'admin-comments',
+     //       'keywords' => array('text')
 
-        ));
-    }
-}
+       // ));
+    //}
+//}
 ?>
