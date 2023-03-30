2a379b11e9b9eef7728256b7c23e50308279df1c
Hacker_Master01 <56979864+BenjaminPet@users.noreply.github.com>
2023-03-30 10:28:23 +0200

    debug 7

--- twentytwentythree/functions.php
+++ twentytwentythree/functions.php
@@ -1,9 +1,11 @@
 <?php
-add_action( 'acf/init', 'hfm_acf_init_block_types' );
-//function register_acf_blocks() {
-//    register_block_type( __DIR__ . '/blocks/text' );
-//}
+add_action( 'init', 'register_acf_block' );
+function register_acf_blocks() {
+   register_block_type( __DIR__ . '/blocks/text' );
+}
 
+
+add_action( 'acf/init', 'hfm_acf_init_block_types' );
 function hfm_acf_init_block_types() {
     if (function_exists('acf_register_block_type')) {
         
