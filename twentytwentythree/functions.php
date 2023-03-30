a1fd4f33c22d009886790cc6f62126bb778a147f
Hacker_Master01 <56979864+BenjaminPet@users.noreply.github.com>
2023-03-30 09:56:23 +0200

    function register gutenburg block

--- twentytwentythree/functions.php
+++ twentytwentythree/functions.php
@@ -1,6 +1,23 @@
 <?php
-add_action( 'init', 'register_acf_blocks' );
-function register_acf_blocks() {
-    register_block_type( __DIR__ . '/blocks/text' );
+//add_action( 'init', 'register_acf_blocks' );
+//function register_acf_blocks() {
+//    register_block_type( __DIR__ . '/blocks/text' );
+//}
+
+function hfm_acf_init_block_types() {
+    if (function_exists('acf_register_block_type')) {
+        
+        // register our custom block
+        scf_register_block_type( array(
+            'name' => 'test',
+            'title' => 'test',
+            'description' => 'test',
+            'render_template' => 'blocks/text/text.php',
+            'category' => 'formatting',
+            'icon' => 'admin-comments',
+            'keywords' => array('text')
+
+        ))
+    }
 }
 ?>
